9aa0c5a10b1ee57a7e9f04f533f26d4a6b25713b
Matthew K <mattkolega58@gmail.com>
2023-07-10 20:21:48 +1000

    Setup notifications page

--- app/(authed)/home.tsx
+++ app/(authed)/home.tsx
@@ -1,12 +1,15 @@
-import { Button, Title, Text } from "react-native-paper";
+import { Button, Title, Text, IconButton } from "react-native-paper";
 import { View } from "react-native";
 import { useAuth } from "../../contexts/auth";
 import { useState } from "react";
 import axios from "axios";
+import { Stack, useNavigation } from "expo-router";
 
 export default function Home() {
   const [message, setMessage] = useState("No message");
   const { logOut } = useAuth();
+
+  const navigation = useNavigation();
 
   const getMessage = async () => {
     try {
@@ -18,11 +21,23 @@ export default function Home() {
   };
 
   return (
-    <View>
-      <Title>Home Page</Title>
-      <Button onPress={logOut}>Log out</Button>
-      <Button onPress={getMessage}>Get Message</Button>
-      <Text>{message}</Text>
-    </View>
+    <>
+      <Stack.Screen 
+          options={{
+            headerTitle: "Home",
+            headerRight: () =>
+              <>
+                <IconButton icon="bell" onTouchStart={() => navigation.navigate("notifications")} />
+                <IconButton icon="cog-outline" onTouchStart={() => navigation.navigate("settings")}/>
+              </>
+          }}
+      />
+      <View>
+        <Title>Home Page</Title>
+        <Button onPress={logOut}>Log out</Button>
+        <Button onPress={getMessage}>Get Message</Button>
+        <Text>{message}</Text>
+      </View>
+    </>
   );
 }
--- app/(authed)/notifications.tsx
+++ app/(authed)/notifications.tsx
@@ -1,10 +1,61 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Avatar, Divider, Text } from "react-native-paper";
+import { Stack } from "expo-router";
+
+type Notification = {
+  name: string;
+  description: string;
+}
+
+const exampleNotifications: Array<Notification> = [
+  {
+    name: "Person 1",
+    description: "has accepted your hiring request."
+  },
+  {
+    name: "Person 2",
+    description: "has commented on your profile."
+  },
+  {
+    name: "Person 3",
+    description: "has fulfilled your request."
+  },
+]
 
 export default function Notifications() {
   return (
-    <View>
-      <Text>Notifications</Text>
-    </View>
+    <>
+      <Stack.Screen 
+          options={{
+            headerTitle: "Notifications"
+          }}
+      />
+      <View>
+        {
+          exampleNotifications.map((notification, index) => 
+          <>
+            <View key={index} style={styles.notificationContainer}>
+              <Avatar.Text label="P" />
+              <Text>{`${notification.name} ${notification.description}`}</Text>
+            </View>
+            <Divider />
+          </>
+          )
+        }
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create ({
+  notificationContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10.0,
+    paddingTop: 10.0,
+    paddingBottom: 10.0,
+    paddingLeft: 5.0,
+    paddingRight: 5.0,
+  }
+});
