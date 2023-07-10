e79249828aa17137952143ad0a4ce2b66c7caded
Matthew K <mattkolega58@gmail.com>
2023-07-10 20:34:16 +1000

    Setup settings layout

--- app/(authed)/settings.tsx
+++ app/(authed)/settings.tsx
@@ -1,10 +1,44 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Button, Divider, Text } from "react-native-paper";
+import { Stack } from "expo-router";
 
 export default function Settings() {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <>
+      <Stack.Screen 
+            options={{
+              headerTitle: "Settings"
+            }}
+      />
+      <View>
+        <Text variant="titleLarge">Account</Text>
+        <View style={styles.settingsButtonContainer}>
+          <Button>Edit Personal Profile</Button>
+          <Divider />
+          <Button>Change Password</Button>
+          <Divider />
+          <Button>Log Out</Button>
+          <Divider />
+          <Button>Delete Account</Button>
+        </View>
+
+        <Text variant="titleLarge">About</Text>
+        <View style={styles.settingsButtonContainer}>
+          <Button>Contact Us</Button>
+          <Divider />
+          <Button>About Us</Button>
+          <Divider />
+          <Button>Terms of Use</Button>
+          <Divider />
+          <Button>Privacy Policy</Button>
+        </View>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  settingsButtonContainer: {
+    backgroundColor: "white"
+  }
+});
