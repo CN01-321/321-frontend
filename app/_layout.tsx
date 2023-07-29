e948dcfdb5784a7f2dd887c5061c20c8d01df2a1
George Bull <geo@exemail.com.au>
2023-07-29 10:58:51 +1000

    added basic colour themes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,46 @@
 import { Slot } from "expo-router";
-import {
-  MD3LightTheme as DefaultTheme,
-  PaperProvider,
-} from "react-native-paper";
+import { MD3LightTheme, PaperProvider } from "react-native-paper";
 import { AuthProvider } from "../contexts/auth";
 
-// here is where the themes settings can be changed
 const theme = {
-  ...DefaultTheme,
-  dark: true,
+  ...MD3LightTheme,
   colors: {
-    ...DefaultTheme.colors,
-    primary: "red",
-    secondary: "yellow",
+    ...MD3LightTheme.colors,
+    // colours of primary objects and containers
+    primary: "#a87351",
+    primaryContainer: "#a87351",
+    // colours of text on primary objects and containers
+    onPrimary: "#ffffff",
+    onPrimaryContainer: "#ffffff",
+
+    secondary: "#a87351",
+    secondaryContainer: "#a87351",
+    onSecondary: "#ffffff",
+    onSecondaryContainer: "#ffffff",
+
+    tertiary: "#a87351",
+    tertiaryContainer: "#a87351",
+    onTertiary: "#ffffff",
+    onTertiaryContainer: "#311300",
+
+    error: "#eb2c2c",
+    errorContainer: "#eb2c2c",
+    onError: "#ffffff",
+    onErrorContainer: "#410002",
+
+    background: "#fcfcfc",
+    onBackground: "#777777",
+
+    elevation: {
+      ...MD3LightTheme.colors.elevation,
+      level0: "transparent",
+      level1: "#ffffff",
+      level2: "#fcfcfc",
+      level3: "transparent",
+      level4: "transparent",
+      level5: "transparent",
+    },
   },
-  background: "red",
 };
 
 export default function Layout() {
