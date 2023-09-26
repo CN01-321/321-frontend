b96e9e2040a3f689c742dea22329c1d6a0a998ae
George Bull <geo@exemail.com.au>
2023-09-26 16:59:13 +1000

    moved index to src and supressed depreciation warning

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { AXIOS_BASE_URL } from "@env";
-import axios from "axios";
-import "expo-router/entry";
-
-axios.defaults.baseURL = AXIOS_BASE_URL;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { AXIOS_BASE_URL } from "@env";
+import { LogBox } from "react-native";
+import axios from "axios";
+import "expo-router/entry";
+
+axios.defaults.baseURL = AXIOS_BASE_URL;
+
+// Ignore Stack deprecation warning as this is a bug in expo router
+// https://github.com/expo/router/issues/834
+LogBox.ignoreLogs([
+  "The `redirect` prop on <Screen /> is deprecated and will be removed. Please use `router.redirect` instead",
+]);
