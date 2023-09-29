cda9ca8b05b1552e1baed83954960515682aa53e
Matthew K <mattkolega58@gmail.com>
2023-09-29 21:56:25 +1000

    Update get-started ui

--- src/app/(auth)/get-started.tsx
+++ src/app/(auth)/get-started.tsx
@@ -1,8 +1,10 @@
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { CARER_COLOUR, OWNER_COLOUR, UserType } from "../../types/types";
 import { Text, Button } from "react-native-paper";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image"
 import Header from "../../components/Header";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function GetStarted() {
   const { userType } = useLocalSearchParams<{ userType: UserType }>();
@@ -11,27 +13,121 @@ export default function GetStarted() {
   const colour = userType === "owner" ? OWNER_COLOUR : CARER_COLOUR;
 
   return (
-    <View>
-      <Header title="Get Started" />
-      <Button
-        mode="contained"
-        onPress={() =>
-          router.push({ pathname: "/(auth)/signup", params: { userType } })
-        }
-        theme={{ colors: { primary: colour } }}
-      >
-        Get Started
-      </Button>
-      <Text>Already registered?</Text>
-      <Button
-        mode="contained"
-        onPress={() =>
-          router.push({ pathname: "/(auth)/login", params: { userType } })
-        }
-        theme={{ colors: { primary: colour } }}
-      >
-        Login
-      </Button>
-    </View>
+    <SafeAreaView>
+      <Header title="Get Started" showHeader={false} />
+      <View style={styles.logoContainer}>
+          <Image
+            style={styles.logoImage}
+            contentFit="contain"
+            source={
+              userType === "owner"
+              ? require("../../../assets/illustrations/logo-yellow.png")
+              : require("../../../assets/illustrations/logo-brown.png")
+            }
+          />
+        </View>
+      <View style={styles.view}>
+        
+        <Text style={styles.heading}>
+          {
+            userType === "owner"
+            ? `Find Loving Sitters\nto Care For your\nFurry Family`
+            : `Earn extra Money\nwith your Love\nforPets`
+          }
+        </Text>
+        <View style={styles.graphicContainer}>
+          <Image
+            style={styles.graphicImage}
+            contentFit="contain"
+            source={
+              userType === "owner"
+              ? require("../../../assets/illustrations/getstarted-owner.png")
+              : require("../../../assets/illustrations/getstarted-carer.png")
+            }
+          />
+        </View>
+        <Button
+          mode="contained"
+          style={styles.button}
+          contentStyle={styles.buttonContent}
+          labelStyle={styles.buttonLabel}
+          onPress={() =>
+            router.push({ pathname: "/(auth)/signup", params: { userType } })
+          }
+          theme={{ colors: { primary: colour } }}
+        >
+          Get Started
+        </Button>
+      </View>
+      <View style={styles.bottomTextContainer}>
+        <Text style={styles.bottomText}>
+          Already have an account? 
+        </Text>
+        <Button 
+          mode="text" 
+          onPress={() => router.push({ pathname: "/(auth)/login", params: { userType } })}
+        >
+          Sign In
+        </Button>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  logoContainer: {
+    width: "35%",
+    height: 60,
+    marginLeft: 30,
+    marginTop: "5%",
+    display: "flex",
+  },
+  logoImage: {
+    width: "100%",
+    height: "100%"
+  },
+  heading: {
+    marginTop: 20,
+    fontFamily: "Montserrat-Bold",
+    fontSize: 24,
+    color: "#312F4A",
+  },
+  graphicContainer: {
+    width: "100%",
+    height: 500,
+    display: "flex",
+    alignItems: "center",
+  },
+  graphicImage: {
+    width: "90%",
+    height: "90%",
+  },
+  button: {
+    borderRadius: 50,
+  },
+  buttonContent: {
+    height: 60,
+  },
+  buttonLabel: {
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 22,
+    color: "#FFFFFF",
+    paddingTop: 5,
+  },
+  bottomTextContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20
+  },
+  bottomText: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 14,
+    color: "#00000094"
+  },
+});
