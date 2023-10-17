7c85f49d391a963ef9567eb309b45ccdafe58161
George Bull <geo@exemail.com.au>
2023-10-18 09:31:33 +1100

    updated jsdoc

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,10 @@
+/**
+ * Entrypoint for the react router app,
+ * returns a blank page as the auth context will re-reoute the
+ * user to the correct path, either the landing page or the home page
+ *
+ * @author George Bull
+ */
 import { View } from "react-native";
 
 export default function Main() {
